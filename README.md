e19f59f5ec2b1b8f8169fdd3f09656df7bc93211
Erik Lissel <erik@lissel.net>
2025-12-03 18:43:44 +0100

    Day3p2

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ Time measured using [hyperfine](https://github.com/sharkdp/hyperfine), running `
 
 **σ** measures the standard-deviation, meaning that 68% of runs are within `mean ± σ`.
 
-| . | Mean time | σ |
+|   | Mean time | σ |
 |---|---|---|
-| Day 1 | 20.1ms | 0.7ms |
-| Day 2 | 115.0 ms | 1.2ms |
+| Day 1 | 20.1 ms | 0.7 ms |
+| Day 2 | 115.0 ms | 1.2 ms |
+| Day 3 | 16.3 ms | 0.4 ms |
--- src/day3.rs
+++ src/day3.rs
@@ -1,13 +1,23 @@
 pub fn day3(lines: Vec<String>) {
-    let p1: u64 = lines.iter().map(|line| find_joltage(line)).sum();
+    let p1: u64 = lines.iter().map(|line| find_joltage(line, 2)).sum();
+    let p2: u64 = lines.iter().map(|line| find_joltage(line, 12)).sum();
     println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 }
 
-fn find_joltage(line: &str) -> u64 {
-    let (ten_index, ten_val) = find_highest(&line[..line.len()-1]);
-    let (_, single_val) = find_highest(&line[(ten_index+1)..]);
+fn find_joltage(line: &str, items: usize) -> u64 {
+    let mut total = 0_u64;
+    let mut string_position = 0_usize;
 
-    return ten_val*10 + single_val;
+    for i in 0..items {
+        let num_position = items-i;
+        let (offset_in_slice, val) = find_highest(&line[string_position..line.len()-num_position+1]);
+        string_position += offset_in_slice + 1;
+        total += val * 10_u64.pow(num_position as u32-1) as u64;
+    }
+
+    println!("{} {}" ,line, total);
+    return total;
 }
 
 fn find_highest(line: &str) -> (usize, u64) {
