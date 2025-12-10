f765c423eb515e1b3c1abe8c77360db4e514a9ca
Erik Lissel <erik@lissel.net>
2025-12-10 23:38:42 +0100

    Day 10p2 holy hell

--- README.md
+++ README.md
@@ -5,16 +5,21 @@ cargo build --release
 hyperfine --warmup 3 --runs 10 './target/release/aoc2025 [day]'
 ```
 
+**REQUIREMENTS FOR DAY10**
+
+- Having **glpsol** available on path, in order to solve GMPL-files
+
 **σ** measures the standard-deviation, meaning that 68% of runs are within `mean ± σ`.
 
-|       | Mean time | σ      |
-|-------|-----------|--------|
-| Day 1 | < 5 ms    | -      |
-| Day 2 | 90.1 ms   | 1.0 ms |
-| Day 3 | < 5 ms    | -      |
-| Day 4 | 9.1 ms    | 0.1 ms |
-| Day 5 | < 5 ms    | - ms   |
-| Day 6 | < 5 ms    | -      |
-| Day 7 | < 5 ms    | -      |
-| Day 8 | 95.0 ms   | 1.2 ms |
-| Day 9 | 61.5 ms   | 0.8 ms |
+|        | Mean time | σ       |
+|--------|-----------|---------|
+| Day 1  | < 5 ms    | -       |
+| Day 2  | 90.1 ms   | 1.0 ms  |
+| Day 3  | < 5 ms    | -       |
+| Day 4  | 9.1 ms    | 0.1 ms  |
+| Day 5  | < 5 ms    | - ms    |
+| Day 6  | < 5 ms    | -       |
+| Day 7  | < 5 ms    | -       |
+| Day 8  | 95.0 ms   | 1.2 ms  |
+| Day 9  | 61.5 ms   | 0.8 ms  |
+| Day 10 | 280.4 ms  | 39.7 ms |
--- src/day10.rs
+++ src/day10.rs
@@ -1,21 +1,86 @@
 use std::collections::VecDeque;
+use std::fs;
+use std::process::Command;
 
 #[derive(Debug)]
 struct Machine {
     state: usize,
     buttons: Vec<usize>,
+    buttons_p2: Vec<Vec<usize>>,
+    joltage: Vec<usize>,
 }
 
 pub fn day10(lines: Vec<String>) {
-    let mut machines = parse_lines(&lines);
-    let mut button_presses = 0;
-    for machine in &mut machines {
-        button_presses += solve(machine);
+    let machines = parse_lines(&lines);
+    let mut button_presses_p1 = 0;
+    let mut button_presses_p2 = 0;
+    let mut count = 0;
+    for machine in &machines {
+        button_presses_p1 += solve_p1(machine);
+        count = count + 1;
+        print_gmpl(machine, count);
+        button_presses_p2 += solve_gmpl(count);
     }
-    println!("Part 1: {}", button_presses);
+    println!("Part 1: {}", button_presses_p1);
+    println!("Part 2: {}", button_presses_p2);
 }
 
-fn solve(machine: &mut Machine) -> usize {
+fn solve_gmpl(line: usize) -> usize {
+    let mut command = Command::new("glpsol");
+    command
+        .arg("--model")
+        .arg(format!("day10-gmpl/{}.mod", { line }));
+    let p = command.output().unwrap();
+    let file = String::from_utf8_lossy(&p.stdout);
+    let lines = file.split("\n").collect::<Vec<&str>>();
+    let second_to_last_line = lines.iter().nth_back(2).unwrap();
+    println!("Step: {}", second_to_last_line);
+    second_to_last_line.parse::<f64>().unwrap().round() as usize
+}
+
+fn print_gmpl(machine: &Machine, count: usize) {
+    let variable_count = machine.buttons_p2.len();
+    let mut s = String::new();
+
+    for i in 0..variable_count {
+        s += &format!("var x{} integer >= 0;\n", i);
+    }
+
+    let objective =
+        (1..variable_count).fold("x0".to_string(), |acc, x| acc + &format!(" + x{}", x));
+    s += &format!("minimize objective: {};\n", objective);
+
+    // subject to constraints: x + y <= 4 and 2 * x + y >= 2.
+    for joltage_index in 0..machine.joltage.len() {
+        let mut vars = Vec::new();
+        for (button_index, button) in machine.buttons_p2.iter().enumerate() {
+            if button.contains(&joltage_index) {
+                vars.push(button_index);
+            }
+        }
+        let lhs = vars
+            .iter()
+            .map(|&b| format!("x{}", b))
+            .collect::<Vec<String>>()
+            .join(" + ");
+
+        s += &format!(
+            "s.t. constraint{}: {} = {};\n",
+            joltage_index + 1,
+            lhs,
+            machine.joltage[joltage_index]
+        );
+    }
+
+    s += "solve;\n";
+
+    s += &format!("printf \"%.4f\\n\", {};", objective);
+    s += "end;";
+    let path = &format!("day10-gmpl/{}.mod", count);
+    fs::write(path, s).expect(&format!("Should be able to write to `{}`", path));
+}
+
+fn solve_p1(machine: &Machine) -> usize {
     let mut frontier = VecDeque::new();
     frontier.push_back((machine.state, 0_usize));
     let mut shortest_distance = vec![9999; 1025];
@@ -43,26 +108,43 @@ fn parse_lines(lines: &[String]) -> Vec<Machine> {
         .map(|l| {
             let (first, rest) = l.split_once(']').unwrap();
             let (mid, end) = rest.split_once('{').unwrap();
+            let (buttons, buttons_p2) = parse_buttons(&mid[1..mid.len() - 1]);
 
             Machine {
                 state: parse_state(&first[1..]),
-                buttons: parse_buttons(&mid[1..mid.len() - 1]),
+                buttons,
+                buttons_p2,
+                joltage: parse_joltage(&end[..end.len() - 1]),
             }
         })
         .collect()
 }
 
-fn parse_buttons(input: &str) -> Vec<usize> {
-    input
-        .split(" ")
-        .map(|s| parse_button(&s[1..s.len() - 1]))
-        .collect()
+fn parse_joltage(input: &str) -> Vec<usize> {
+    input.split(',').map(|n| n.parse().unwrap()).collect()
+}
+
+fn parse_buttons(input: &str) -> (Vec<usize>, Vec<Vec<usize>>) {
+    (
+        input
+            .split(" ")
+            .map(|s| parse_button(&s[1..s.len() - 1]))
+            .collect(),
+        input
+            .split(" ")
+            .map(|s| parse_button_p2(&s[1..s.len() - 1]))
+            .collect(),
+    )
 }
 
 fn parse_button(input: &str) -> usize {
     input.split(',').fold(0, |total, strnum| {
         total + 2_usize.pow(strnum.parse().unwrap())
     })
+}
+
+fn parse_button_p2(input: &str) -> Vec<usize> {
+    input.split(',').map(|num| num.parse().unwrap()).collect()
 }
 
 fn parse_state(s: &str) -> usize {
